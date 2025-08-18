0e3edf4372e7af764bde28281a5462eadbadace7
Nandhishwaran01 <nandhishwaran777k@gmail.com>
2025-08-18 10:53:58 +0530

    +

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-	origin: process.env.CLIENT_URL,
+	origin: process.env.CLIENT_URL,localhost:3000,
+	methods: ["GET", "POST", "PUT", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"],
 	credentials: true,
 }));
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://fashion-ecommerce-brg1.onrender.com" : "/api",
+	baseURL: import.meta.mode === "production" ? "https://fashion-ecommerce-brg1.onrender.com" : "/api",
 	withCredentials: true, // send cookies to the server
 });
 
