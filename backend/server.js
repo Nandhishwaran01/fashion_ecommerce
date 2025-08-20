290751f0da45720f7597e25590b0328dfb75dc70
Nandhishwaran <k.nandhishwaran777k@gmail.com>
2025-08-20 13:31:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:3000", 
-  "https://fashion-ecommerce.vercel.app"
+  "https://fashion-ecommerce.vercel.app/api"
 ];
 
 app.use(cors({
